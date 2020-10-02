ebd5a0aa3811cfb12120aa3867e1661857cf85a3
mohamed gamal <m.yassen.93@gmail.com>
2020-10-02 16:18:31 +0200

    aida_week3

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import User
 
-# Create your views here.
+
+def loginPage(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        user = User.objects.create_volunteeruser(email=email, first_name=first_name, last_name=last_name,
+                                                 address=address, password=password, phone=phone)
+        if user is not None:
+            login(request, user)
+            return redirect('login')
+        else:
+            messages.add_message(request, messages.error, 'Please Review Your Data Failed To Register')
+    context = {}
+    return render(request, 'accounts/register.html', context)
